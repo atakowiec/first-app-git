6cebcb229db1104618c7698bdf672eb46e9de8f7
mrdrozdy <mrdrozdowskii@gmail.com>
2024-10-17 13:59:26 +0200

    feat:dashboard

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,12 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Dashboard dashboard = new Dashboard();
+
+        dashboard.addLoggedInUser("JanKowalski");
+        dashboard.addLoggedInUser("AnnaNowak");
+
+        dashboard.displayLoggedInUsers();
+
+        dashboard.resetDashboard();
     }
 }
